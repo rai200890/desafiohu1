55c307fca8422dfc5cddbb9feebca8463bf2ca60
Raissa Ferreira <rai200890@gmail.com>
2015-09-25 19:05:47 -0300

    MySQL install script.

---
+++ hu_api/install_mysql.sh
@@ -0,0 +1,7 @@
+#MySQL Installation
+export MYSQL_ROOT_USERNAME=root
+export MYSQL_ROOT_PASSWORD=root
+sudo apt-get -y install debconf-utils
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password '$MYSQL_ROOT_PASSWORD
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password '$MYSQL_ROOT_PASSWORD
+sudo apt-get -y install mysql-server
